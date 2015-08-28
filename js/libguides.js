8b570544620587aaa1f224f185dcc051f777871a
Matthew Reidsma <reidsmam@gvsu.edu>
2015-08-28 15:21:36 -0400

    Added new ebook tracking javascript

--- js/libguides.js
+++ js/libguides.js
@@ -1,29 +1,31 @@
-var ebookProvs = ["ACLS Humanities E-Book Project","African American Experience","American Indian Experience","Cambridge Companions","Cambridge Histories Online","Daily Life Through History","Early English Books Online (EEBO)","Ebooks at Ebscohost","Ebrary","EditLib Digital Library","Eighteenth Century Collections Online","Knovel","Latino American Experience","Oxford Scholarship Online","Pop Culture Universe","Safari Books Online","StarWalk KidsMedia","Stat!Ref","World Folklore and Folklife"];
+$(document).ready(function() {
+	var ebookProvs = ["ACLS Humanities E-Book Project","African American Experience","American Indian Experience","Cambridge Companions","Cambridge Histories Online","Daily Life Through History","Early English Books Online (EEBO)","Ebooks at Ebscohost","Ebrary","EditLib Digital Library","Eighteenth Century Collections Online","Knovel","Latino American Experience","Oxford Scholarship Online","Pop Culture Universe","Safari Books Online","StarWalk KidsMedia","Stat!Ref","World Folklore and Folklife"];
 
-function isInArray(value, array) {
-  return array.indexOf(value) > -1;
-}
+	function isInArray(value, array) {
+	  return array.indexOf(value) > -1;
+	}
 
-if($('.s-lg-az-result-title').length > 0) {
+	if($('.s-lg-az-result-title').length > 0) {
 
-	console.log('This is a database page');
+		console.log('This is a database page');
 
-	// Database list
+		// Database list
 
-	$('.s-lg-az-result-title').each(function() {
+		$('.s-lg-az-result-title').each(function() {
 
-		var linkText = $(this).find('a').text();
-		if(isInArray(linkText, ebookProvs)) {
+			var linkText = $(this).find('a').text();
+			if(isInArray(linkText, ebookProvs)) {
 
-			console.log('Adding click handler to databases...');
-			$(this).find('a').click(function() {
+				console.log('Adding click handler to databases...');
+				$(this).find('a').click(function() {
 
-				var ebookProvider = $(this).text();
-				console.log(linkText);
-				var eBookTracker = document.createElement('img');
-				eBookTracker.src = '//labs.library.gvsu.edu/labs/ebooks/?source=databases&prov=' + encodeURIComponent(linkText.trim());
-				document.body.appendChild(eBookTracker);
-			});
-		}
-	});	
-}
+					var ebookProvider = $(this).text();
+					console.log(linkText);
+					var eBookTracker = document.createElement('img');
+					eBookTracker.src = '//labs.library.gvsu.edu/labs/ebooks/?source=databases&prov=' + encodeURIComponent(linkText.trim());
+					document.body.appendChild(eBookTracker);
+				});
+			}
+		});	
+	}
+});
