e7cb4c2853ba58a38243b4082ea7b7d5d624b438
Matthew Reidsma <reidsmam@gvsu.edu>
2015-08-28 15:16:01 -0400

    Added new ebook tracking javascript

---
+++ js/libguides.js
@@ -0,0 +1,25 @@
+var ebookProvs = ["ACLS Humanities E-Book Project","African American Experience","American Indian Experience","Cambridge Companions","Cambridge Histories Online","Daily Life Through History","Early English Books Online (EEBO)","Ebooks at Ebscohost","Ebrary","EditLib Digital Library","Eighteenth Century Collections Online","Knovel","Latino American Experience","Oxford Scholarship Online","Pop Culture Universe","Safari Books Online","StarWalk KidsMedia","Stat!Ref","World Folklore and Folklife"];
+
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
+}
+
+if($('.s-lg-az-result-title').length > 0) {
+
+	// Database list
+
+	$('.s-lg-az-result-title').each(function() {
+
+		var linkText = $(this).find('a').text();
+		if(isInArray(linkText, ebookProvs)) {
+			$(this).find('a').click(function() {
+
+				var ebookProvider = $(this).text();
+				console.log(linkText);
+				var eBookTracker = document.createElement('img');
+				eBookTracker.src = '//labs.library.gvsu.edu/labs/ebooks/?source=databases&prov=' + encodeURIComponent(linkText.trim());
+				document.body.appendChild(eBookTracker);
+			});
+		}
+	});	
+}
